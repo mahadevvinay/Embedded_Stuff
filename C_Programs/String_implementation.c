49b45238dcc84c3fafe1b970cceaeceeb505c30d
Vinay <c_vhunac@qti.qualcomm>
2016-11-21 18:38:27 +0530

    Toupper and ToLower function

--- C_Programs/String_implementation.c
+++ C_Programs/String_implementation.c
@@ -26,15 +26,28 @@ int main()
         char *s1, *s2;
         int n;
         s1 = (char*)malloc(sizeof(char)*1000);
+        s2 = (char*)malloc(sizeof(char)*1000);
         scanf("%s",s1);
+        scanf("%s",s2);
 //      printf("%s\n", s1);	// testing purpose
 //      printf("%s\n",s2);     // tesing purpose
 
+/* Testing Upper and Lower Case function */
+        printf("\nTo upper case:");
         while(*s1)
         {
                 char ch = ToUpper(*s1++);
                 printf("%c",ch);
         }
+        printf("\nTo Lower case:");
+        while(*s2)
+        {
+                char ch = ToLower(*s2++);
+                printf("%c",ch);
+        }
+        printf("\n");
+
+
 
 }
 
